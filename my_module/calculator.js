797da816bffc8a09e5d06f5de2bbee13f62edd45
leinadiyanu <106745348+leinadiyanu@users.noreply.github.com>
2025-05-04 00:24:25 +0100

    Create calculator.js

---
+++ my_module/calculator.js
@@ -0,0 +1,4 @@
+export let add = (number1, number2) => {return number1 + number2;}
+export let subtract = (number1, number2) => {return number1 - number2;}
+export let multiply = (number1, number2) => {return number1 * number2;}
+export let divide = (number1, number2) => {return number1 / number2;}
